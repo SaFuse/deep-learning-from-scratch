b7952904d899cd9563c4f11fa0b9100109c34125
SaFuse <fuse.satoshi.26a@st.kyoto-u.ac.jp>
2017-09-04 18:00:33 +0900

    show results on matplotlib

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -2,6 +2,7 @@
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
+import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from PIL import Image
 
@@ -10,7 +11,7 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False, one_hot_label = True)
 
 img = x_train[0]
 label = t_train[0]
@@ -19,5 +20,6 @@ print(label)  # t_train[0]と5が対応している
 print(img.shape)  # (784,)
 img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
 print(img.shape)  # (28, 28)
-
-img_show(img)
+plt.imshow(img)
+plt.show()
+# img_show(img)
